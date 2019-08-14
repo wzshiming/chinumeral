6072c07cb7e01e2f3242627a5a6b214d2bc6b54d
wzshiming <wzshiming@foxmail.com>
2019-08-14 10:47:36 +0800

    fixes

--- chinese_test.go
+++ chinese_test.go
@@ -6,35 +6,33 @@ import (
 )
 
 func TestChinese(t *testing.T) {
-	for i := Chinese(0); i != 1e6; i++ {
-		tmp, err := i.EncodeToString(Lower)
-		if err != nil {
-			t.Error(err)
-		}
-		var d Chinese
-		_, err = d.DecodeString(tmp)
-		if err != nil {
-			t.Error(err)
-		}
-		if i != d {
-			t.Fatal(uint64(i), tmp, uint64(d))
-		}
+	startd := [][2]Chinese{
+		{0, 1e6 + 1e5},
+		{1e7 - 1e5, 1e7 + 1e5},
+		{1e8 - 1e5, 1e8 + 1e5},
+		{1e9 - 1e5, 1e9 + 1e5},
+		{1e10 - 1e5, 1e10 + 1e5},
+		{1e11 - 1e5, 1e11 + 1e5},
+		{1e12 - 1e5, 1e12 + 1e5},
+		{1e13 - 1e5, 1e13 + 1e5},
+		{1e14 - 1e5, 1e14 + 1e5},
+		{math.MaxUint64 - 1e5, math.MaxUint64},
 	}
 
-	for i := Chinese(0); i != 1e6; i++ {
-		j := i + math.MaxUint64
-		tmp, err := j.EncodeToString(Lower)
-		if err != nil {
-			t.Error(err)
-		}
-		var d Chinese
-		_, err = d.DecodeString(tmp)
-		if err != nil {
-			t.Error(err)
-		}
-		if j != d {
-			t.Fatal(uint64(j), tmp, uint64(d))
+	for _, s := range startd {
+		for i := s[0]; i != s[1]; i++ {
+			tmp, err := i.EncodeToString(Lower)
+			if err != nil {
+				t.Error(err)
+			}
+			var d Chinese
+			_, err = d.DecodeString(tmp)
+			if err != nil {
+				t.Error(err)
+			}
+			if i != d {
+				t.Fatal(uint64(i), tmp, uint64(d))
+			}
 		}
 	}
-
 }
